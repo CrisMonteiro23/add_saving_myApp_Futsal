cb8eba5b0217a15fb3b664ea7e19242167d0d32c
CrisMonteiro23 <cristianjmonteiro@gmail.com>
2025-08-11 10:03:17 +0200

    Update jugador.dart

--- lib/models/jugador.dart
+++ lib/models/jugador.dart
@@ -6,4 +6,18 @@ class Jugador {
     required this.id,
     required this.nombre,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'nombre': nombre,
+    };
+  }
+
+  factory Jugador.fromJson(Map<String, dynamic> json) {
+    return Jugador(
+      id: json['id'],
+      nombre: json['nombre'],
+    );
+  }
 }
